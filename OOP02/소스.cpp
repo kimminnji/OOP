20af5e587ab55d5258ecfdfa171d1176db490243
민지 김 <feather.lh@gmail.com>
2019-09-09 10:30:21 +0900

    qaqaq

---
+++ OOP02/소스.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int maain() {
+	std::cout << "Hello World!" << std::endl;
+
+	system("pause"); //콘솔 화면이 바로 사라짐 방지
+	return 0;
+}
---
+++ OOP02/소스1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class LetDebug {
+public:
+	void printNum() {
+		int s1 = 32767;
+		int s2 = 1;
+		int s3 = (int)s1 + s2;
+		std::cout << s3 << std::endl;
+	}
+};
+
+int main() {
+	LetDebug* ld = new LetDebug;
+	ld->printNum();
+	system("pause");
+	return 0;
+}
